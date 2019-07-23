7cd679841c261ecf2b538df5ae97de3239620188
Vladimir Iakimov <yakimovvn@bk.ru>
2019-07-23 18:28:37 +0300

    Создал классы из задания.

---
+++ src/MainTask/Race.java
@@ -0,0 +1,17 @@
+package MainTask;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+
+    private ArrayList<Stage> stages;
+
+    public ArrayList<Stage> getStages() {
+        return stages;
+    }
+
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+}
